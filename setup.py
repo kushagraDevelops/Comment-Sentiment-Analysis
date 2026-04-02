57143680dc28cd24030da1a492db96dc4fa117e1
kushagracoder2005 <kushagras049@gmail.com>
2026-04-03 00:11:57 +0530

    added setup.pu

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A small chrome plugin to detect youtube comment sentiments',
+    author='CampusX',
+    license='',
+)
+
+
+
+
